3790e5b68c1ec417a3150b8456b1216ca3ca615b
Kanza Shahid <kanza.shahid@teo-intl.com>
2024-05-26 23:26:33 +0500

    notFound component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
