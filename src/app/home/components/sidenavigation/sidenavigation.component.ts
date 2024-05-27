7def6e0c19286af3609e9b851ec003c9727db5ff
Kanza Shahid <kanza.shahid@teo-intl.com>
2024-05-28 00:17:14 +0500

    sideNavigation created

---
+++ src/app/home/components/sidenavigation/sidenavigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenavigation',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidenavigation.component.html',
+  styleUrl: './sidenavigation.component.scss'
+})
+export class SidenavigationComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { CategorynavigationComponent } from './components/categorynavigation/categorynavigation.component';
+import { SidenavigationComponent } from './components/sidenavigation/sidenavigation.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CategorynavigationComponent],
+  imports: [
+    HeaderComponent,
+    CategorynavigationComponent,
+    SidenavigationComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
